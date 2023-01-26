5d549ec5e5414cae4243f5f1669098fd0ebc9a2b
Himalayan <hdnpress@gmail.com>
2023-01-26 16:49:42 +0545

    Blog post may have many comments & establish relationship and seed the data

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->catchPhrase(),
+            'body' => fake()->realText(),
+            'created_at' => fake()->dateTimeBetween('-20 days', '-10 days'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Comment;
 use App\Models\Company;
 use App\Models\Employee;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -22,6 +25,11 @@ class DatabaseSeeder extends Seeder
         Company::factory(10)->create()->each(function ($company) {
             $company->employees() // relationship method in Company.php
                 ->saveMany(Employee::factory(mt_rand(2, 5))->make());
+        });
+
+        Post::factory(10)->create()->each(function ($post) {
+            $post->comments()
+                ->saveMany(Comment::factory(mt_rand(2, 5))->make());
         });
     }
 }
